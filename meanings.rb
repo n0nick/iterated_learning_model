f619a7332f1df05ae087db7014a7c91dbee8e75e
Sagie Maoz <sagie@maoz.info>
2013-09-18 19:17:06 +0300

    Generate all 100 meanings from Kirby 2000

--- meanings.rb
+++ meanings.rb
@@ -1,5 +1,21 @@
-Meanings = [
-  :mary_loves_john,
-  :john_loves_mary,
+# encoding: UTF-8
+
+MeaningObjects = [
+  :Mike, :John, :Mary, :'Tu ̈nde', :Zoltan
 ]
+
+MeaningActions = [
+  :Loves, :Knows, :Hates, :Likes, :Finds
+]
+
+Meanings = []
+
+MeaningObjects.each do |agent|
+  MeaningActions.each do |predicate|
+    (MeaningObjects - [agent]).each do |patient|
+      sentence = "#{agent}_#{predicate}_#{patient}"
+      Meanings << sentence.to_sym
+    end
+  end
+end
 
