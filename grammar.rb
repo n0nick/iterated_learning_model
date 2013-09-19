cdbefae8d64aae61bbd969a2a2812cba0bef3fd6
Sagie Maoz <sagie@maoz.info>
2013-09-19 12:43:55 +0300

    Grammar supports reverse lookup

--- grammar.rb
+++ grammar.rb
@@ -1,14 +1,21 @@
 class Grammar < Hash
   def initialize
     super
+    @reverse = Hash.new
   end
 
   def learn(word, meaning)
+    @reverse[meaning.to_sym] ||= []
+    @reverse[meaning.to_sym] << word
     self[word.to_sym] = meaning
   end
 
   def lookup(word)
     self[word.to_sym]
+  end
+
+  def reverse_lookup(meaning)
+    @reverse[meaning.to_sym]
   end
 end
 
