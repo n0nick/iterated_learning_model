4b7e7141f7f7259c34b34954e6ce0b9114cff5ba
Sagie Maoz <sagie@maoz.info>
2013-09-19 11:32:04 +0300

    Grammar store words by meaning parts, too

--- grammar.rb
+++ grammar.rb
@@ -1,5 +1,16 @@
 class Grammar < Hash
+  def initialize
+    super
+    @by_part = {}
+  end
+
   def learn_meaning(word, meaning)
+    meaning.each do |part, value|
+      @by_part[part] ||= {}
+      @by_part[part][value] ||= []
+      @by_part[part][value] << word
+    end
+
     self[word.to_sym] = meaning
   end
 
