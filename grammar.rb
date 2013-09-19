50fdad25af202b94b9054df681dd92e2563d0f6f
Sagie Maoz <sagie@maoz.info>
2013-09-19 15:27:56 +0300

    Reverse order of grammar meaning->word
    Clearer Grammar::Item.to_s

--- grammar.rb
+++ grammar.rb
@@ -1,25 +1,33 @@
 class Grammar < Hash
+  class Item
+    attr_accessor :meaning, :word
+
+    def initialize(meaning, word)
+      self.meaning = meaning
+      self.word = word
+    end
+
+    def to_s
+      "'#{word}'"
+    end
+  end
+
   attr_reader :meanings_count
 
   def initialize
     @meanings_count = 0
   end
 
-  def learn(word, meaning)
+  def learn(meaning, word)
     @meanings_count+= 1 if meaning.full?
 
-    self[word.to_sym] = meaning
+    self[meaning.to_sym] = Item.new(meaning, word)
   end
 
-  def lookup(word)
-    self[word.to_sym]
-  end
-
-  def reverse_lookup(target)
-    self.select do |word, meaning|
-      target.matches?(meaning)
+  def lookup(target)
+    self.select do |key, item|
+      target.matches?(item.meaning)
     end
   end
 end
-
 
