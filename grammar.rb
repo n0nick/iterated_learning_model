50f0e59fc47385c72a93fd54bb43a611c6628400
Sagie Maoz <sagie@maoz.info>
2013-09-24 17:48:24 +0300

    Add docs to grammar.rb, make merge_part private

--- grammar.rb
+++ grammar.rb
@@ -55,6 +55,7 @@ class Grammar < Hash
     end
   end
 
+  # learn a new rule
   def learn(meaning, word=nil)
     rule = nil
 
@@ -67,37 +68,19 @@ class Grammar < Hash
     add_rule(rule) unless rule.nil?
   end
 
+  # find all rules with same part=meaning
   def with(part, meaning)
     values.select do |rule|
       rule.meaning[part] == meaning
     end
   end
 
+  # merge parts of a given rule
   def merge(rule)
     rule.meaning.each do |part, meaning|
       if rule.meaning.has?(part)
         new_rule = merge_part(rule.meaning[part], part)
-        learn new_rule unless new_rule.nil?
-      end
-    end
-  end
-
-  def merge_part(meaning, part)
-    rules = with(part, meaning)
-    if rules.count > 1
-      words = rules.map { |r| r.word }
-      new_word = Utils.longest_common_substr words, /[0-9]/
-
-      unless new_word.empty?
-        rules.each do |r|
-          delete_rule(r)
-          r.generalise_part! part, new_word
-          add_rule(r)
-        end
-
-        new_meaning = Meaning.new
-        new_meaning[part] = meaning
-        Rule.new(new_meaning, new_word)
+        learn(new_rule) unless new_rule.nil?
       end
     end
   end
@@ -114,7 +97,7 @@ class Grammar < Hash
       # remove unrealistic recursive rules "1 -> 1a"
       if rule.partial?
         if rule.meaning.unknown_parts.count <= 1
-          delete key
+          delete_rule rule
         end
       end
     end
@@ -124,6 +107,7 @@ class Grammar < Hash
     end
   end
 
+  # find all rules matching a meaning
   def lookup(target)
     select do |key, rule|
       target.matches?(rule.meaning)
@@ -132,20 +116,58 @@ class Grammar < Hash
 
   private
 
+  # add a rule to grammar
   def add_rule rule
     self[rule.meaning.to_sym] = rule
   end
 
+  # remove a rule from grammar
   def delete_rule rule
     delete rule.meaning.to_sym
   end
 
+  # merge all rules with same part=meaning
+  def merge_part(meaning, part)
+    rules = with(part, meaning)
+
+    if rules.count > 1
+      words = rules.map { |r| r.word }
+      new_word = Utils.longest_common_substr words, /[0-9]/
+
+      unless new_word.empty?
+        # generalise that part in all rules
+        rules.each do |r|
+          delete_rule(r)
+          r.generalise_part! part, new_word
+          add_rule(r)
+        end
+
+        # create new rule for that part=meaning
+        new_meaning = Meaning.new
+        new_meaning[part] = meaning
+        Rule.new(new_meaning, new_word)
+      end
+    end
+  end
+
+  # split a single-part rule
+  #   part=1, other=2, food=Banana -> 1moz2
+  # into two rules: one for meaning, one for structure
+  #   food=Banana -> 1moz2
+  #   part=1, other=2, food=3 -> 132
   def split_single_rule rule
+    # rule has a single part
     part = rule.meaning.known_parts.first
-    new_word = rule.literal
-    new_meaning = Meaning.new
+
+    # new rule for the single meaning
+    new_word = rule.literal # ('moz')
+    new_meaning = Meaning.new # (food=Banana)
     new_meaning[part] = rule.meaning[part]
-    add_rule Rule.new(new_meaning, new_word)
+    new_rule = Rule.new(new_meaning, new_word) # (food=Banana -> moz)
+    add_rule(new_rule)
+
+    # generalize the original rule
+    #  (part=1, other=2, food=3 -> 132)
     rule.generalise_part! part, new_word
     rule
   end
